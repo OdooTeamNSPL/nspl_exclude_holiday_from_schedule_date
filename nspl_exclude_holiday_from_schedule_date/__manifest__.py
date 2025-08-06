ba13d2a070d683f0c64da8697a12d55b0c34cbfd
Team Odoo NSPL <odoo_team@namahsoftech.com>
2025-08-06 14:49:32 +0530

    Add files via upload

---
+++ nspl_exclude_holiday_from_schedule_date/__manifest__.py
@@ -0,0 +1,36 @@
+{
+    'name': 'Delivery Restriction on Holidays and Weekends',
+    'version': '17.0',
+    'summary':
+        """
+  Auto-adjusts delivery dates in sales orders to skip weekends and holidays, ensuring accurate, business-day-based scheduling.
+
+    """,
+    'description': """
+    ✔ Automatically skips weekends and public holidays when calculating delivery dates  
+    ✔ Ensures delivery schedules are based only on working business days  
+    ✔ Respects configured company holidays and product/customer lead times  
+    ✔ Improves delivery accuracy and customer satisfaction
+
+    This module adjusts sales order delivery dates by excluding weekends and configured company holidays. If a computed delivery date falls on a non-working day, it is automatically moved to the next business day, ensuring reliable and accurate delivery scheduling.
+    """,
+
+    'category': 'Sales',
+    'sequence': 2,
+    'author': 'Namah Softech Private Limited',
+    'website': 'https://www.namahsoftech.com/',
+    'license': 'OPL-1',
+    'price': 55.99,
+    'currency': 'USD',
+    'support': 'support@namahsoftech.com',
+    'contributors': ["Rutik Patil"],
+    'depends': ['base', 'sale_management', 'account', 'stock'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/company_holiday.xml',
+    ],
+    'images': ['static/description/img/banner.png'],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
